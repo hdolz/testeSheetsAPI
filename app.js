ad5c524ee0a7ecc44c38dc85c62b110d402e488c
Henrique Dolzane <henriquedolzane@gmail.com>
2019-10-03 15:27:29 -0400

    modificacao basica de api sheet  para minicurso

--- app.js
+++ app.js
@@ -22,19 +22,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    console.log('Requisição GET -> / : '+getDataEHora());
+    console.log('Requisição GET -> / : ' + getDataEHora());
     console.log(' -> Redirecionado para index da aplicação');
     res.render('index');
 });
 
 app.get('/candidatos', (req, res) => {
-    console.log('Requisição GET -> /candidatos : '+getDataEHora());
+    console.log('Requisição GET -> /candidatos : ' + getDataEHora());
     console.log(' -> Redirecionado para página de consulta candidatos');
     res.render('candidatos');
 });
 
-app.get('/cadastros',(req,res)=>{
-    console.log('Requisição GET -> /cadastros : '+getDataEHora());
+app.get('/cadastros', (req, res) => {
+    console.log('Requisição GET -> /cadastros : ' + getDataEHora());
     (async function accessSpreadSheet() {
         const doc = new GoogleSpreadSheet(process.env.SHEET_ID);
         await promisify(doc.useServiceAccountAuth)(creds);
@@ -43,21 +43,21 @@ app.get('/cadastros',(req,res)=>{
         const rows = await promisify(sheet.getRows)({
             offset: 1
         })
-        console.log(' -> Dados retornados de spreadsheet: '+process.env.SHEET_ID);
-        console.log(' -> Quantidade de dados: '+rows.length);
+        console.log(' -> Dados retornados de spreadsheet: ' + process.env.SHEET_ID);
+        console.log(' -> Quantidade de dados: ' + rows.length);
         res.send(rows);
     })()
 });
 
-app.get('/chatbot',(req,res)=>{
-    console.log('Requisição GET -> /chatbot : '+getDataEHora());
+app.get('/chatbot', (req, res) => {
+    console.log('Requisição GET -> /chatbot : ' + getDataEHora());
     console.log(' -> Redirecionado para página de chatbot');
     res.render('chatbot');
 });
 
 //insere candidato por meio de JSON no corpo da requisição 
 app.post('/inserirDados', (req, res) => {
-    console.log('Requisição POST -> /inserirDados : '+getDataEHora());
+    console.log('Requisição POST -> /inserirDados : ' + getDataEHora());
     console.log(' -> Requisição externa');
     (async function inserirDados() {
         const doc = new GoogleSpreadSheet(process.env.SHEET_ID);
@@ -67,7 +67,7 @@ app.post('/inserirDados', (req, res) => {
         await promisify(sheet.addRow)({
             Nome: req.body.Nome,
             Email: req.body.Email,
-            DtNasc: req.body.DtNasc,
+            //DtNasc: req.body.DtNasc,
             Interesse: req.body.Interesse
         });
         console.log(' -> Objeto recebido: ');
@@ -87,7 +87,7 @@ app.post('/inserirDados', (req, res) => {
 });
 
 app.post('/inserir', (req, res) => {
-    console.log('Requisição POST -> /inserir : '+getDataEHora());
+    console.log('Requisição POST -> /inserir : ' + getDataEHora());
     console.log(' -> Requisição via formulário');
     const row = {
         Nome: req.body.nome,
@@ -110,11 +110,11 @@ app.listen(porta, () => {
     console.log(`Servidor ouvindo na porta ${porta}`);
 });
 
-function formatarDataPadraoBR(dataPadraoUS){
+function formatarDataPadraoBR(dataPadraoUS) {
     return dataPadraoUS.split('-').reverse().join('/');
 }
 
-function getDataEHora(){
+function getDataEHora() {
     let data = new Date();
     return `Data: ${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()} - ${data.getHours()}:${data.getMinutes()}`;
 }
